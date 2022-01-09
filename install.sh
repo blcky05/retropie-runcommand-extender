b59c58f9d08cf1a906da2a66b438f9d0fe46d99f
Christian Heupel <c_heupel15@cs.uni-kl.de>
2022-01-09 18:06:43 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+TMP=`realpath $0`
+REPO_PATH=`dirname $TMP`/
+
+SCRIPT_PATH=/opt/retropie/configs/all/
+EXTENDER_SCRIPT=execute_runcommand_scripts
+ONSTART_SCRIPT=runcommand_onstart
+ONLAUNCH_SCRIPT=runcommand_onlaunch
+ONEND_SCRIPT=runcommand_onend
+
+EXTENDER_SCRIPT_PATH=${SCRIPT_PATH}${EXTENDER_SCRIPT}.sh
+ONSTART_SCRIPT_PATH=${SCRIPT_PATH}${ONSTART_SCRIPT}.sh
+ONLAUNCH_SCRIPT_PATH=${SCRIPT_PATH}${ONLAUNCH_SCRIPT}.sh
+ONEND_SCRIPT_PATH=${SCRIPT_PATH}${ONEND_SCRIPT}.sh
+
+# only continue if the install script has not already been executed
+if [ -f "$EXTENDER_SCRIPT_PATH" ]; then
+    echo "$EXTENDER_SCRIPT_PATH already exists. Exiting."
+	exit 0
+fi
+
+# move existing scripts
+if [ -f "$ONSTART_SCRIPT_PATH" ]; then
+    mv $ONSTART_SCRIPT_PATH ${SCRIPT_PATH}${ONSTART_SCRIPT}-0.sh
+fi
+
+if [ -f "$ONLAUNCH_SCRIPT_PATH" ]; then
+    mv $ONLAUNCH_SCRIPT_PATH ${SCRIPT_PATH}${ONLAUNCH_SCRIPT}-0.sh
+fi
+
+if [ -f "$ONEND_SCRIPT_PATH" ]; then
+    mv $ONEND_SCRIPT_PATH ${SCRIPT_PATH}${ONEND_SCRIPT}-0.sh
+fi
+
+# symlink the extender scripts
+ln -s ${REPO_PATH}${EXTENDER_SCRIPT}.sh $EXTENDER_SCRIPT_PATH
+ln -s ${REPO_PATH}${ONSTART_SCRIPT}.sh $ONSTART_SCRIPT_PATH
+ln -s ${REPO_PATH}${ONLAUNCH_SCRIPT}.sh $ONLAUNCH_SCRIPT_PATH
+ln -s ${REPO_PATH}${ONEND_SCRIPT}.sh $ONEND_SCRIPT_PATH
